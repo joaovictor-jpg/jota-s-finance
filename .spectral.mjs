b194db5f2bbe523ecdf1c7a474a458dc5278a75a
joaovictormdasilva676@gmail.com <joaovictormdasilva676@gmail.com>
2025-04-10 23:44:41 -0300

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3ODIwMg/spectral.js?branch_slug=main&token=79f88d9a-aaf0-47b9-ae9d-2dda7ef3ddd6";
+export default { extends: ruleset };
